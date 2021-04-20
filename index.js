7e8ec703c7432135adb516b48a5a06751f0f46b2
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-19 23:17:30 -0700

    commit tree 1fee9f8d080ef0ca67aebedc8e8423c739c9a7a5

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email){
+    let userInfo = {name: name, email: email} 
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+        }
+return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            let idText = document.createTextNode(object.id)
+            let p = document.createElement('p')
+            p.append(idText)
+            const script = document.querySelector("body")
+            script.append(p)
+        })
+        .catch(function(error){
+            let text = document.createTextNode(error.message)
+            let pg = document.createElement('p')
+            pg.append(text)
+            const script = document.querySelector("body")
+            script.append(pg)
+        })
+}
